89958271821a1d0e4b49b97ae821a599b37a2689
abilmazhitova <a_abilmazhitova@mail.ru>
2024-10-29 18:14:06 +0500

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -6,10 +6,3 @@
     window.byeSpeaker = byeSpeaker; 
 })(); 
  
-Файл SpeakHello.js: 
-(function () {    var helloSpeaker = {}; 
-    var speakWord = "Hello"; 
-    helloSpeaker.speak = function(name) {        console.log(speakWord + " " + name); 
-    }; 
-    window.helloSpeaker = helloSpeaker; 
-})(); 
